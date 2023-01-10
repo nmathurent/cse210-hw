ed38dfd03a2f61f2d545b4b200aa31eddd8cc2ad
nmathurent <mathurentnelson@gmail.com>
2023-01-10 00:07:53 -0800

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
 
+
 class Program
 {
+    // Function that Displays the message, "Welcome to the Program!"
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // Function that Asks for and returns the user's name (as a string)
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        string userN = Console.ReadLine();
+        return userN;
+    }
+
+    // Function that Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        return int.Parse(userInput);
+    }
+
+    // Function that Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int numberParam)
+    {
+        return numberParam * numberParam;
+    }
+
+    // Function that Accepts the user's name and the squared number and displays them.
+    static void DisplayResult (string userName, int numberParam)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {numberParam}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
     }
 }
+
